efc218f54f7b5319fd573d96aa464e3accfa478c
John Netzel <johnrnetzel@gmail.com>
2022-04-05 22:08:27 -0800

    Added heirarchy to items to tree view

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,24 +2,38 @@ const treeViewWidgetElement = $("#tree-view-widget")
 
 function populateTreeViewWidget()
 {
-    const item1 = generateTreeViewWidget("Item 1")
-    const item2 = generateTreeViewWidget("Item 2")
-    const item3 = generateTreeViewWidget("Item 3")
-    const item4 = generateTreeViewWidget("Item 4")
-    const item5 = generateTreeViewWidget("Item 5")
+    const item1 = generateTreeViewWidget("Item 1");
+    const item2 = generateTreeViewWidget("Item 2");
+    const item3 = generateTreeViewWidget("Item 3");
+    const item4 = generateTreeViewWidget("Item 4");
+    const item5 = generateTreeViewWidget("Item 5");
+    const item6 = generateTreeViewWidget("Item 6");
+    const item7 = generateTreeViewWidget("Item 7");
     treeViewWidgetElement.append(item1);
-    treeViewWidgetElement.append(item2);
+    /*treeViewWidgetElement.append(item2);
     treeViewWidgetElement.append(item3);
     treeViewWidgetElement.append(item4);
-    treeViewWidgetElement.append(item5);
+    treeViewWidgetElement.append(item5);*/
+    addChildWidgets(item1, [item2, item3, item4]);
+    addChildWidgets(item2, [item5, item6]);
+    addChildWidgets(item4, [item7]);
 }
 
 function generateTreeViewWidget(name)
 {
     const content = $("<div>");
-    content.append(name + "<ul><li>Item 1</li><li>Item 2</li><li>Item 3</li></ul>");
+    content.append(name + "<ul></ul>");
     content.addClass("tree-view-content");
     return content;
+}
+
+function addChildWidgets(parent, children)
+{
+    const parentList = parent.children("ul");
+    for(child of children)
+    {
+        parentList.append(child);
+    }
 }
 
 populateTreeViewWidget();
