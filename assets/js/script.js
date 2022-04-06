f00edba8e428262b62d56c5533edc33e706e7287
John Netzel <johnrnetzel@gmail.com>
2022-04-05 21:24:49 -0800

    Add list to content inside tree view

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,15 @@ const treeViewWidgetElement = $("#tree-view-widget")
 
 function populateTreeViewWidget()
 {
+    treeViewWidgetElement.append(generateTreeViewWidget());
+}
+
+function generateTreeViewWidget()
+{
     const content = $("<div>");
-    content.append("ABCD");
+    content.append("Head<ul><li>ListItem1</li><li>ListItem2</li><li>ListItem3</li></ul>");
     content.addClass("tree-view-content");
-    treeViewWidgetElement.append(content);
+    return content;
 }
 
 populateTreeViewWidget();
