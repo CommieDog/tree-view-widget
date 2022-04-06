ac864c2d315d5b917c2ca72b315f3a3c7dddd761
John Netzel <johnrnetzel@gmail.com>
2022-04-06 01:44:24 -0800

    Format expand icon elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,7 +24,7 @@ function generateTreeViewWidget(name)
     const content = $("<div>");
     const expandIcon = $("<div>");
 
-    expandIcon.append("+");
+    expandIcon.append("&nbsp;+&nbsp;");
     expandIcon.addClass("expand-icon");
 
     content.append(expandIcon);
